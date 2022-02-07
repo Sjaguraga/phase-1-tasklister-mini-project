32a666311816187b976d559e542f8911d4ae2547
Serref Jaguraga <62582673+Sjaguraga@users.noreply.github.com>
2022-02-06 20:23:07 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,3 @@
-/*document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector('form')
-  form.addEventListener('submit' (e)=>{
-  e.preventDefault()
-  form.reset()
-  })
-});
-
-function buildToDo(toDo){
-let p = document.createElement('p')
-let btn = document.createElement('button')
-btn.addEventListener('click', handleDelete)
-p.textContent = toDo
-p.appendChild(btn)
-console.log('p')
-document.querySelector('#create-task-form').appendChild(p)
-}
-
-function handleDelete(e){
-  e.target.parentNode.remove()
-}*/
-
-
-
-
-
 const newTaskDescription = document.getElementById("new-task-description");
 const theList = document.getElementById("tasks");
 
